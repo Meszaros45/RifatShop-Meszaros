0649a996d827994646343e28973f7d086c8ae0b1
Mariano Meszaros <meszarosmariano@outlook.com>
2022-08-22 19:18:54 -0300

    Segundo Commit modificado

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Miprimercomponente from './components/Miprimercomponente';
+import NavBar from './components/NavBar/NavBar'; 
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <Miprimercomponente />
+        <div className='cuadrado'></div>
+        <div style={{ width: '150px', height: '150px', backgroundColor:'blue'}}></div>
       </header>
     </div>
   );
